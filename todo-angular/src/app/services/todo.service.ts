7c1059a4e5ebbc5bec457d6e7a0143acdad64232
DEEPAK KUMAR <dpkchauhan1994@gmail.com>
2017-12-13 18:56:23 +0530

    todo-angular service file changes

--- todo-angular/src/app/services/todo.service.ts
+++ todo-angular/src/app/services/todo.service.ts
@@ -4,8 +4,16 @@ import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import { Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 
+//RxJS operator for mapping the observable
+import 'rxjs/add/operator/map'
+
 @Injectable()
 export class ToDoService{
     api_url = 'http://localhost:3000';
-    todoUrl = 
+    todoUrl = `${this.api_url}/api/todos`;
+
+    constructor(private _http: HttpClient){
+        
+    }
+
 }
